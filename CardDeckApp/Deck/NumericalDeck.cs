f5ec11080853b996bd0830f3cdcecdaffd1a1fc8
AlexDelgado0792 <arekkusu0792@gmail.com>
2019-04-30 15:34:31 -0500

    refactoring of standard deck

---
+++ CardDeckApp/Deck/NumericalDeck.cs
@@ -0,0 +1,28 @@
+﻿using Deck.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Deck
+{
+    public abstract class NumericalDeck
+    {
+        public IDictionary<StandardSuit, IList<int>> MyDeck = new Dictionary<StandardSuit, IList<int>>()
+        {
+            { StandardSuit.Clubs, Enumerable.Range(1, 13).Select(x => x).ToList() },
+            { StandardSuit.Diamonds, Enumerable.Range(1, 13).Select(x => x).ToList() },
+            { StandardSuit.Hearts, Enumerable.Range(1, 13).Select(x => x).ToList() },
+            { StandardSuit.Spades, Enumerable.Range(1, 13).Select(x => x).ToList() }
+        };
+
+        public virtual void Draw()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CardsLeft()
+        {
+            return MyDeck.Values.Sum(x => x.Count()) > 0;
+        }
+    }
+}
--- CardDeckApp/Deck/StandardDeck.cs
+++ CardDeckApp/Deck/StandardDeck.cs
@@ -5,16 +5,8 @@ using System.Linq;
 
 namespace Deck
 {
-    public class StandardDeck
+    public class StandardDeck : NumericalDeck
     {
-        public IDictionary<StandardSuit, IList<int>> Deck = new Dictionary<StandardSuit, IList<int>>()
-        {
-            { StandardSuit.Clubs, Enumerable.Range(1, 13).Select(x => x).ToList() },
-            { StandardSuit.Diamonds, Enumerable.Range(1, 13).Select(x => x).ToList() },
-            { StandardSuit.Hearts, Enumerable.Range(1, 13).Select(x => x).ToList() },
-            { StandardSuit.Spades, Enumerable.Range(1, 13).Select(x => x).ToList() }
-        };
-
         public IDictionary<StandardSuit, IList<int>> PlayedCards = new Dictionary<StandardSuit, IList<int>>()
         {
             { StandardSuit.Clubs, new List<int>() },
@@ -25,7 +17,7 @@ namespace Deck
 
         private readonly Random random = new Random();
 
-        public void Draw()
+        public override void Draw()
         {
             var suit = (StandardSuit)random.Next(0, 4);
             int number = random.Next(1, 14);
@@ -37,15 +29,10 @@ namespace Deck
             }
             else
             {
-                Console.WriteLine($"{Deck.Values.Sum(x => x.Count())} Left => {ValueToName(number)}-{suit.ToString()} played.");
-                Deck[suit].Remove(number);
+                Console.WriteLine($"{MyDeck.Values.Sum(x => x.Count())} Left => {ValueToName(number)}-{suit.ToString()} played.");
+                MyDeck[suit].Remove(number);
                 PlayedCards[suit].Add(number);
             }
-        }
-
-        public bool CardsLeft()
-        {
-            return Deck.Values.Sum(x => x.Count()) > 0;
         }
 
         private bool AlreadyPlayed(StandardSuit suit, int number)
