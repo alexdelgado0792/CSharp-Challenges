b68488a6c7878acc0e7ac16d16c6b0ae004f2087
AlexDelgado0792 <arekkusu0792@gmail.com>
2019-04-30 15:34:43 -0500

    add low high game card

---
+++ CardDeckApp/Deck/LowHighDeck.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Deck
+{
+    public class LowHighDeck : NumericalDeck
+    {
+
+        public override void Draw()
+        {
+            
+        }
+    }
+}
