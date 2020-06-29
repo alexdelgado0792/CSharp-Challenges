bb4296c7672418c461f6c216eb8811b694c61c7f
AlexDelgado0792 <arekkusu0792@gmail.com>
2020-06-29 13:07:41 -0500

    Pair of sum
    upload project solution

---
+++ PairSum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace PairSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1, 2, 3, 4 };
+            var a = HasPair(numbers, 8);
+            int[] numbers2 = new int[] { 1, 2, 4, 4 };
+            var b = HasPair(numbers2, 8);
+        }
+
+        private static bool HasPair(IEnumerable<int> numbers, int sum)
+        {
+            var complements = new List<int>();
+
+            foreach (var value in numbers)
+            {
+                if (complements.Contains(value))
+                {
+                    return true;
+                }
+                complements.Add(sum - value);
+            }
+
+
+            return false;
+        }
+    }
+}
